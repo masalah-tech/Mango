1b03837f033b212cb1cef347ed9426b137201f3c
Mazen Salah <masalah.tech@gmail.com>
2024-04-17 20:16:27 +0300

    #

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,8 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mango.Web.Controllers
 {
@@ -31,6 +33,37 @@ namespace Mango.Web.Controllers
         public async Task<IActionResult> CouponCreate()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["Success"] = "Coupon created successfully";
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+
+            TempData["Error"] = "Something went wrong";
+            return View();
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+			ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+			}
+
+			return NotFound();
         }
     }
 }
